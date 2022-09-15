0afacf2009326fcf3c11bd1b6a20030d48d13161
Roopesht <roopesht@gmail.com>
2022-09-15 21:16:33 +0530

    initial checkin

---
+++ tests/test_compare.py
@@ -0,0 +1,20 @@
+import unittest
+#from testdata1 import *
+class test_compare(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp() 
+        #testdata = testdata1.data1
+        #print(testdata)
+
+    #@unittest.TestCase('is down')
+    def test_compare_isdown(self):
+        #self.fail('it always fails')
+        pass
+
+    #@unittest.TestCase ('is up')
+    def test_compare_isup(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+    
