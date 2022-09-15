f9d8f2c0560fafe4c050c7fb1e05a0a50811e813
Roopesht <roopesht@gmail.com>
2022-09-15 21:25:33 +0530

    updated test data, added last row

--- tests/testdata1.py
+++ tests/testdata1.py
@@ -1,2 +1,3 @@
 data1 = [{'open': 101, 'high': 103, 'low': 99, 'close': 100, 'volume': 1000, 'date': '2022-09-01 09:15'},
-         {'open': 100, 'high': 102, 'low': 98, 'close': 99, 'volume': 500, 'date': '2022-09-01 09:16'},]
+         {'open': 100, 'high': 102, 'low': 98, 'close': 99, 'volume': 500, 'date': '2022-09-01 09:16'},
+         {'open': 99, 'high': 101, 'low': 97, 'close': 98, 'volume': 1000, 'date': '2022-09-01 09:17'},]
